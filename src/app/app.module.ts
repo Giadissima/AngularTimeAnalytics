9f66d0712ca5d2f1834069f99fb3cc74d9815321
Giadissima <52421987+Giadissima@users.noreply.github.com>
2023-08-29 18:37:04 +0200

    added datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
+import { AlarmsBarChartComponent } from './dashboard/charts/alarms-bar-chart/alarms-bar-chart.component';
+import { AlarmsPieChartComponent } from './dashboard/charts/alarms-pie-chart/alarms-pie-chart.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartsComponent } from './dashboard/charts/charts.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {DpDatePickerModule} from 'ng2-date-picker';
+import { DpDatePickerModule } from 'ng2-date-picker';
 import { NgModule } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { TestComponent } from './test/test.component';
-import { ToolbarComponent } from './dashboard/toolbar/toolbar.component';
-import { AlarmsBarChartComponent } from './dashboard/charts/alarms-bar-chart/alarms-bar-chart.component';
 import { PeopleBarChartComponent } from './dashboard/charts/people-bar-chart/people-bar-chart.component';
 import { PeoplePieChartComponent } from './dashboard/charts/people-pie-chart/people-pie-chart.component';
-import { AlarmsPieChartComponent } from './dashboard/charts/alarms-pie-chart/alarms-pie-chart.component';
+import { TestComponent } from './test/test.component';
+import { ToolbarComponent } from './dashboard/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
+    DashboardComponent, 
     TestComponent,
     ChartsComponent,
     ToolbarComponent,
